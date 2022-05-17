3e7dd5acc356dedb215374a475f734458cca9386
Felipe Alves <felipejean2002@gmail.com>
2022-05-17 05:37:26 -0300

    docs: Adicionando JSDoc

--- admin/js/editor/Editor.History.js
+++ admin/js/editor/Editor.History.js
@@ -1,11 +1,27 @@
-Editor.History = class {
+/**
+ * Grava, refaz e desfaz ações feitas pelo editor. 
+ */
+class EditorHistory {
+    /**
+     * @type {Editor}
+     */
     #editor;
+    
+    /**
+     * @param {Editor} editor 
+     */
     constructor(editor) {
         this.#editor = editor;
+        
         this.timeline = [];
         this.now = -1;
     }
 
+    /**
+     * "Rebobina" o histórico para o tempo de número index.
+     * @param {number} index - O número da ação, por ex: a primeira ação tem número 0, etc.
+     * @returns {void}
+     */
     rewind(index) {
         if (this.now < index) {
             this.redo();
@@ -30,11 +46,21 @@ Editor.History = class {
         
         this.#editor.trigger("historyChange", this.#editor);
     }
-
+    
+    /**
+     * Verifica se a ação atual do histórico pode ser desfeita.
+     * @public
+     * @returns {boolean}
+     */
     undoIsDisabled() {
         return this.timeline.length > 0 && this.now > -1;
     }
-
+    
+    /**
+     * Desfaz a ação atual.
+     * @public
+     * @returns {void}
+     */
     undo() {
         this.timeline[this.now].undo();
         this.timeline[this.now].always();
@@ -46,11 +72,21 @@ Editor.History = class {
         
         this.#editor.trigger("historyChange", this.#editor);
     }
-
+    
+    /**
+     * Verifica se a ação atual do histórico pode ser refeita.
+     * @public
+     * @returns {boolean}
+     */
     redoIsDisabled() {
         return this.now == (this.timeline.length - 1) || this.timeline.length == 0;
     }
-
+    
+    /**
+     * Refaz a ação atual.
+     * @public
+     * @returns {void}
+     */
     redo() {
         this.now += 1;
         if (this.now > (this.timeline.length - 1))
@@ -62,17 +98,40 @@ Editor.History = class {
         this.#editor.trigger("historyChange", this.#editor);
     }
 
+    /**
+     * Verifica se o histórico pode ser limpado.
+     * @public
+     * @returns {boolean}
+     */
     clearIsDisabled() {
         return this.timeline.length == 0;
     }
 
+    /**
+     * Limpa o histórico por completo.
+     * @public
+     * @returns {void}
+     */
     clear() {
         this.timeline = [];
         this.now = 0;
         this.#editor.trigger("historyChange", this.#editor);
     }
 
-    add({ description, undo, redo, always = () => { } }) {
+    /**
+     * Adiciona uma ação ao histórico.
+     * @public
+     * @param {{
+     *     description: string,
+     *     redo: (...any) => void,
+     *     undo: (...any) => void,
+     *     always: (...any) => void
+     * }} props 
+     * @returns {void}
+     */
+    add(props) {
+        const { description, undo, redo, always = () => { } } = props;
+        
         if (this.now < (this.timeline.length - 1))
             this.timeline.splice(this.now, this.timeline.length - (this.now + 1));
 
